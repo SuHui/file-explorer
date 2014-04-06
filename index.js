520f3fe8f3b3b82f5b23abb6536807ca44e6c882
suhui <moliyatihui@gmail.com>
2014-04-06 20:40:38 +0800

    重构  + 读文件

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var fs =require('fs');
+var  fs =require('fs')
+	,stdin =process.stdin
+	,stdout=process.stdout;
+var  stats =[];
+
+
+
 
 fs.readdir(process.cwd(), function(err, files){
 	console.log('');
@@ -10,22 +16,53 @@ fs.readdir(process.cwd(), function(err, files){
 		var filename = files[i];
 
 		fs.stat(__dirname + '/' + filename,function(err,stat){
+			stats[i] = stat;
 			if (stat.isDirectory()) {
 				console.log('       '+ i +'    \033[36m' + filename + '/\033[39m');
 			} else{
-				console.log('       '+ i +'    \033[33m' + filename + '\033[39m');
+				console.log('       '+ i +'    \033[90m' + filename + '\033[39m');
 			};
 
-			i++;
-			if (i == files.length) {
-				console.log('');
-				process.stdout.write('       \033[33mEnter your choice:  \033[39m');
-				process.stdin.resume();
-				process.stdin.setEncoding('utf8');
+			if (++i == files.length) {
+				read();
 			} else{
 				file(i);
 			};
 		});
+	}
+	function read(){
+		console.log('');
+		stdout.write('       \033[33mEnter your choice:  \033[39m');
+		stdin.resume();
+		stdin.setEncoding('utf8');
+		stdin.on('data',option);
+
+	}
+
+	function option(data){
+		var filename =files[Number(data)];
+		if (!filename) {
+		stdout.write('       \033[33mEnter your choice:  \033[39m');
+		} else{
+			stdin.pause();
+			if (stats[Number(data)].isDirectory()) {
+				fs.readdir(__dirname + '/' +filename,function(err,files){
+					console.log('');
+					console.log('    (' + files.length +'files) ');
+					files.forEach(function(file){
+						console.log('   -   ' +file);
+					});
+					console.log('');
+				});
+			} else{
+				fs.readFile(__dirname + '/' +filename, 'utf8',function(err,data){
+					console.log('');
+					console.log('\033[99m' +data.replace(/(.*)/g, '      $1') + '\033[39m'   );
+				})
+
+			};
+			
+		};
 	}
 	file(0);
 });
